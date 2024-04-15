f503685f378f85a5e9576a839767f695821de386
JesyMiray <aleksandrow.dima8@gmail.com>
2024-04-15 18:33:10 +0300

    search and delete queries

--- server.js
+++ server.js
@@ -1,39 +1,52 @@
-const express = require('express');
-const {MongoClient} = require('mongodb');
+const express = require("express");
+const {MongoClient, ObjectId} = require("mongodb");
 
 const app = express();
 const port = 8000;
 
-app.use(express.static('public'));
+app.use(express.json());
+app.use(express.static("public"));
 
-app.get('/tasks', async function(req, res) {	
-	const client = new MongoClient('mongodb://127.0.0.1');
+app.get("/tasks", async function(req, res) {	
+	const client = new MongoClient("mongodb://127.0.0.1");
 	await client.connect();
-	const db = client.db('todoapp');
-	const collection = db.collection('tasks');
+	const db = client.db("todoapp");
+	const collection = db.collection("tasks");
 	const data = await collection.find({}).toArray();
 	res.send(data);
 });
 
-app.get('/tasks:id', function(req, res) {
+app.get("/tasks/:id", async function(req, res) {
+	const client = new MongoClient("mongodb://127.0.0.1");
+	await client.connect();
+	const db = client.db("todoapp");
+	const collection = db.collection("tasks");
+	const data = await collection.findOne({_id: new ObjectId(req.params.id)});
+	res.send(data);
 });
 
-app.post('/tasks',async function(req, res) {
-	const task = {};
-	const client = new MongoClient('mongodb://127.0.0.1');
+app.post("/tasks",async function(req, res) {
+	const task = {...req.body, done: false};
+	const client = new MongoClient("mongodb://127.0.0.1");
 	await client.connect();
-	const db = client.db('todoapp');
-	const collection = db.collection('tasks');
+	const db = client.db("todoapp");
+	const collection = db.collection("tasks");
 	await collection.insertOne(task);
 	res.send(task);
 });
 
-app.patch('/tasks:id', function(req, res) {
+app.patch("/tasks/:id", function(req, res) {
 });
 
-app.delete('/tasks:id', function(req, res) {
+app.delete("/tasks/:id", async function(req, res) {
+	const client = new MongoClient("mongodb://127.0.0.1");
+	await client.connect();
+	const db = client.db("todoapp");
+	const collection = db.collection("tasks");
+	await collection.deleteOne({_id: new ObjectId(req.params.id)});
+	res.send({});
 });
 
 app.listen(port, function() {
-	console.log('server started!');
+	console.log("server started!");
 });
